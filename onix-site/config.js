d3b902fd3898e798bc7ca56e9a3f4d572b4fe81a
brunovalim74-rgb <brunovalim74@gmail.com>
2026-09-24 15:27:11 -0300

    Update config.js

--- onix-site/config.js
+++ onix-site/config.js
@@ -3,7 +3,7 @@ const CAMPANHA = {
   meta: 14000,
   distanciaTotal: 789,
   valorKm: 17.74,
-  valorArrecadado: 0, // <-- altere este número quando entrar dinheiro
+  valorArrecadado: 9500.00, // <-- altere este número quando entrar dinheiro
   chavePix: "",       // preencher depois
   favorecidoPix: "", // preencher depois
   whatsapp: "5515996674055",
