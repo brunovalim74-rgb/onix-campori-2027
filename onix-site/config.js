42fa32bb40a2a51e9f8cc5dcc48518e24f9800ad
brunovalim74-rgb <brunovalim74@gmail.com>
2026-09-24 16:31:48 -0300

    Update config.js

--- onix-site/config.js
+++ onix-site/config.js
@@ -3,9 +3,10 @@ const CAMPANHA = {
   meta: 14000,
   distanciaTotal: 789,
   valorKm: 17.74,
-  valorArrecadado: 0, // <-- altere este número quando entrar dinheiro
+  valorArrecadado: 0, // use ponto para centavos: 1250.50
+  ultimaAtualizacao: "24/09/2026",
 
-  // Cole aqui o link oficial da contribuição no 7me quando estiver disponível.
+  // Cole entre as aspas o link oficial da contribuição no 7me.
   link7me: "",
 
   whatsapp: "5515996674055",
