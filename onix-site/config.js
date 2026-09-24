e543fca62aa44c4c2d7638711181c71dead49d21
brunovalim74-rgb <brunovalim74@gmail.com>
2026-09-24 16:17:12 -0300

    Update config.js

--- onix-site/config.js
+++ onix-site/config.js
@@ -3,9 +3,11 @@ const CAMPANHA = {
   meta: 14000,
   distanciaTotal: 789,
   valorKm: 17.74,
-  valorArrecadado: 9500.00, // <-- altere este número quando entrar dinheiro
-  chavePix: "",       // preencher depois
-  favorecidoPix: "", // preencher depois
+  valorArrecadado: 0, // <-- altere este número quando entrar dinheiro
+
+  // Cole aqui o link oficial da contribuição no 7me quando estiver disponível.
+  link7me: "",
+
   whatsapp: "5515996674055",
   instagram: "desbravadores_onix_sorocaba"
 };
